f4cd62e17cdde4843a76148c2866d178b066f324
peti <petya.antal@gmail.com>
2024-11-20 14:18:50 +0100

    tetris kinézet szinten fejlődik

---
+++ tetris/tetris.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+const grid = document.querySelector('.grid');
+let squares = document.querySelectorAll(".grid div")
+const ScoreDisplay = document.querySelector("#score")
+
+const StartBtn = document.querySelector("#start-button")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  for (let i = 0; i < 200; i++) {
+    const div = document.createElement('div');  // Create a diveket
+    grid.appendChild(div);  
+    
+    
+    div.style.border = "1px solid black"
+    
+}
+
+
+}
+
+squares[0].style.backgroundColor = "black"
+
+
+)
