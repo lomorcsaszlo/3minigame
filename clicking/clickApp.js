be619a9104dc422f807b055eb70541cfe0c43289
lomorcsaszlo <laszlo.csomor22@gmail.com>
2024-11-20 14:11:28 +0100

    clicker game

--- clicking/clickApp.js
+++ clicking/clickApp.js
@@ -1,69 +1,110 @@
-/* addEventListener("DOMContentLoaded", function(){
-    
+
+addEventListener("DOMContentLoaded", function(){
+    isThereAnyHS()
 })
 
- */
-function StartGame(){
-    circleAppearing()
-    CircleDissapear()
-    timer()
+
+
+const startBut = document.querySelector(".but");
+const text = document.querySelector(".textIGuess");
+
+startBut.addEventListener("click", function() {
+    text.style.display = "none";
+    StartGame();
+});
+
+function isThereAnyHS(){
+    if(localStorage.getItem("High Score") != undefined){
+        highScore();
+    }
 }
 
-/* Start Button: */
-const startBut = document.querySelector(".but")
-const text = document.querySelector(".textIGuess")
-startBut.addEventListener("click", function(){
+function StartGame() {
+    const startBut = document.querySelector(".but");
+    startBut.style.display = "none"
+    count = 0; 
+    circleAppearing(); 
+    CircleDissapear();
+    timer();
     
-    text.style.display = "none"
-    StartGame()
-})
+}
 
-/* -_-_-_-_-_-_ */
 
 function randomColor() {
-    return `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`;
+    return `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`; /* Color check not to be same as background */
 }
 
 
+function randomPositionTop() {
+    return `${Math.floor(Math.random() * 90)}%`;
+}
 
-function randomPositionTop(){
-    return `${Math.floor(Math.random() * 90)}%`
+function circleAppearing() {
+    const circle = document.querySelector(".circle");
+    circle.style.backgroundColor = randomColor();
+    circle.style.display = "block";
+    circle.style.top = randomPositionTop();
+    circle.style.left = randomPositionTop();
 }
 
 
+let count = 0;
 
-function circleAppearing(){
-    const circle = document.querySelector(".circle")
-    circle.style.backgroundColor = randomColor()
-    circle.style.display = "block"
-    circle.style.top = randomPositionTop()
-    circle.style.left = randomPositionTop()
+function CircleDissapear() {
+    const circle = document.querySelector(".circle");
+    const countWrite = document.querySelector(".count");
+    countWrite.innerHTML = count;
+
+
+    circle.addEventListener("click", function() {
+        if (circle.style.display === "none") return;
+
+        circle.style.display = "none";
+        count += 1;
+        countWrite.innerHTML = count;
+        circleAppearing();
+    });
 }
-var count = 0
-function CircleDissapear(){
-    const circle = document.querySelector(".circle")
-    const countWrite = document.querySelector(".count")
-    countWrite.innerHTML= count
-    circle.addEventListener("click", function(){
-        circle.style.display = "none"
-        count += 1
-        countWrite.innerHTML= count
-        circleAppearing()
-        
-        
 
 
-    })
+function end() {
+    alert("Score: " + count);
+    if (count > localStorage.getItem("High Score")){
+        localStorage.setItem("High Score", count);
+    }
+
     
+    const circle = document.querySelector(".circle");
+    circle.style.display = "none";  
+
+    
+    const newCircle = circle.cloneNode(true); 
+    circle.parentNode.replaceChild(newCircle, circle);  
+
+    const startBut = document.querySelector(".but");
+    startBut.style.display = "block"
 }
-function end(){
-    alert("Score: " + count)
-}
 
-
-
-function timer(){
+function timer() {
     setTimeout(() => {
-        end()
-      }, "4000");
+        end();  
+        highScore();  
+    }, 4000); 
 }
+
+
+function highScore() {
+    const Hs = document.querySelector("#HS");
+    const paste = document.querySelector(".paste");
+    Hs.style.display = "block";
+    Hs.innerHTML = "High Score: " + localStorage.getItem("High Score");
+    paste.style.display = "block";
+}
+
+
+
+
+const pasteHs =  document.querySelector(".paste")
+pasteHs.addEventListener("click", function(){
+    navigator.clipboard.writeText("My high score is: " + localStorage.getItem("High Score"))
+})
