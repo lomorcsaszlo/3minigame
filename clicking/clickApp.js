46b7caf126357fada46cd8533a2df645ab17d2ac
lomorcsaszlo <laszlo.csomor22@gmail.com>
2024-11-27 19:00:18 +0100

    clicking retroing

--- clicking/clickApp.js
+++ clicking/clickApp.js
@@ -40,7 +40,7 @@ function randomPositionTop() {
 
 function circleAppearing() {
     const circle = document.querySelector(".circle");
-    circle.style.backgroundColor = randomColor();
+    
     circle.style.display = "block";
     circle.style.top = randomPositionTop();
     circle.style.left = randomPositionTop();
