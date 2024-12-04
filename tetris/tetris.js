58f4b19114b6e8b4e42720c2a969fc5b617c0637
peti <petya.antal@gmail.com>
2024-12-04 12:56:50 +0100

    VERSATILE MOSMÁ megyön, space, downkey (donkey)

--- tetris/tetris.js
+++ tetris/tetris.js
@@ -1,42 +1,33 @@
 document.addEventListener('DOMContentLoaded', ()=>{
 const grid = document.querySelector('.grid');
-
+let mytime = 500;
 const ScoreDisplay = document.querySelector("#score")
-
 const StartBtn = document.querySelector("#start-button")
   for (let i = 0; i < 200; i++) {
     const div = document.createElement('div');  // Create a diveket
     grid.appendChild(div);  
     div.style.border = "1px solid black"
-    
 }
-
 let squares = document.querySelectorAll(".grid div")
 let i = 0 //19 sor van, ez az sorok száma
 var horizont = 4 //9 oszlop van 
 let protectedsquares = [];
 
-function canIcolorIt(){
+function canIcolorIt(){ //törli azokat az elemeket, amiken nincsen rajta a class
   squares.forEach(element => {
-  
     if (!element.classList.contains("anyád")){
-    
       element.style.backgroundColor = "" //szín reset, de csak ha az a szín még nem ért le.
-        
     }
-
 })}
-
-
+//a lefelé mozgás
 function inovelo(){
   if (i < 20 && squares[parseInt(`${i}${horizont}`)].style.backgroundColor != "black"){
     squares[parseInt(`${i}${horizont}`)].style.backgroundColor = "black"
     canIcolorIt()
   }
   else{
-    squares[parseInt(`${i-1}${horizont}`)].style.backgroundColor = "black"
-
-    squares[parseInt(`${i-1}${horizont}`)].classList.add("anyád")
+    squares[parseInt(`${i-1}${horizont}`)].style.backgroundColor = "black"//ez az előző blokk beszínezése miatt kell, fluidabb így
+    squares[parseInt(`${i-1}${horizont}`)].classList.add("anyád")//ez már nem törölhető canicolorit-tel
     i = 0 //sorok, oszlopok resetelése
     horizont = 4 
   }
@@ -44,6 +35,7 @@ function inovelo(){
   i++
   return i
 }
+//különböző gomblenyomások esetén viselkedés
 function left() {
   if (squares[parseInt(`${i}${horizont-1}`)].style.backgroundColor != "black"){
     horizont -= 1
@@ -60,19 +52,23 @@ function right() {
   squares[parseInt(`${i-1}${horizont}`)].style.backgroundColor = "black"
   }
 }
-document.addEventListener('keydown', function(event) {
-  if (event.key === 'ArrowLeft') {
-      left()
-      //a balra meg lett nyomva
+function down() {
+  i = inovelo();
+}
+function space() {
+  while((i < 20 && squares[parseInt(`${i}${horizont}`)].style.backgroundColor != "black")){
+    i = inovelo()
   }
+  i = inovelo()
+}
+document.addEventListener('keydown', (event) => {
+  if (event.key === 'ArrowLeft') left();
+  if (event.key === 'ArrowRight') right();
+  if (event.key === 'ArrowDown') down();
+  if (event.key === ' ') space();
 });
-document.addEventListener('keydown', function(event) {
-  if (event.key === 'ArrowRight') {
-      right()
-      //a jobbra meg lett nyomva
-  }
-});
-  setInterval(() => {
-      i = inovelo();//folyamatos futás 
-  }, 1000);
+
+setInterval(() => {
+  i = inovelo();//folyamatos futás 
+}, mytime);
 })
