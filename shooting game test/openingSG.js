7b2332e18a3a342fc0ecc241144fdc43ac8eb786
blLilili <biboklilivbj@gmail.com>
2024-12-18 04:51:23 +0100

    "starting screens"

---
+++ shooting game test/openingSG.js
@@ -0,0 +1,24 @@
+function vibration(){
+    const press = document.querySelector(".press")
+    setInterval(function(){
+        if(press.style.display == "none"){
+            press.style.display = "block"
+        }else{
+            press.style.display = "none"
+        }
+    }, 400)
+    
+}
+
+
+document.addEventListener('keydown', function(event) {
+    if (event.key === ' ' || event.key === ' ') {
+        window.open("html.html");
+    }
+});
+
+
+
+vibration()
+
+/* window.open("https://www.example.com", "_blank"); */
